47d0911176145a580e6bc784cdf52431c3e85173
PsykoDev <45910905+PsykoDev@users.noreply.github.com>
2021-09-01 23:24:13 +0200

    FollowMouse (#10)
    * Create readme.md
    
    * Add files via upload
    
    * Update readme.md
    
    * Update readme.md
    
    * Create Script
    
    * Delete Script
    
    * Add files via upload
    
    * Update readme.md
    
    * Update FPS.cs
    
    Changed for OnGUI
    
    * Update readme.md
    
    * Updateuh
    
    * mieux
    
    * Add files via upload
    
    * Update FollowMouse.cs
    
    Co-authored-by: Alexandre2BI <42917206+alex1600@users.noreply.github.com>

---
+++ UNITY/FollowMouse/Script/FollowMouse.cs
@@ -0,0 +1,30 @@
+using UnityEngine;
+
+public class FollowMouse : MonoBehaviour {
+    public float speed = 8.0f;
+    Vector2 lastMousePosition;
+    private bool invoke;
+    [SerializeField]private Camera camera;
+
+    void FixedUpdate() {
+        if((Vector2)Input.mousePosition != lastMousePosition) {
+            lastMousePosition = (Vector2)Input.mousePosition;
+            Vector2 mousePosition = Input.mousePosition;
+            Vector2 mouseScreenToWorld = camera.ScreenToWorldPoint(mousePosition);
+            Vector2 position = Vector2.Lerp(transform.position, mouseScreenToWorld, 1.0f - Mathf.Exp(-speed * Time.deltaTime));
+            transform.position = position;
+        } else if(!invoke) {
+            Invoke(nameof(WhenMouseIsntMoving), .1f);
+            invoke = true;
+        }
+    }
+
+    void WhenMouseIsntMoving() {
+        invoke = false;
+    }
+
+    void OnGUI() {
+        GUI.skin.label.fontSize = 15;
+        GUILayout.Label("Screen: " + Input.mousePosition.ToString());
+    }
+}
---
+++ UNITY/FollowMouse/readme.md
@@ -0,0 +1,7 @@
+Track our mouse and print actial position 2D only 
+
+# HOW TO USE:
+Right Click > PsykoDev > FollowMouse in hierarchy
+
+# WARNING
+No Warn
