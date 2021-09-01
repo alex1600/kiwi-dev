77bd4d616fcc1f6c95fe3a4a51b0494c36bdc2e6
PsykoDev <45910905+PsykoDev@users.noreply.github.com>
2021-09-02 01:33:49 +0200

    AI Play Game

---
+++ AI/Python/AI Play Game/predict.py
@@ -0,0 +1,9 @@
+# PsykoDev
+import numpy as np
+from scipy.misc import imresize
+
+
+def predict(model, X):
+    X = imresize(X, (150, 150, 3)).astype('float32')/255.
+    Y = model.predict(X.reshape(1,150,150,3))
+    return Y
