e4d727d88d4927df2c3bb12aac43ac7ccd9c7830
Alexandre <abelletdu38@gmail.com>
2021-09-05 22:09:35 +0200

    move scripts and add readme c# extensions/utils

---
+++ C#/Extensions/EnumExtensions.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.ComponentModel;
+using System.Collections.Generic;
+using System.Linq;
+
+public static class EnumExtensions
+{
+	/// <summary>
+	/// Returns an enumerator description attribute if present, else the enum as string.
+	/// </summary>
+	public static string GetDescription(this Enum en)
+	{
+		var fi = en.GetType().GetField(en.ToString());
+		if (fi == null) return en.ToString();
+		var attributes = (DescriptionAttribute[])fi.GetCustomAttributes(typeof(DescriptionAttribute), false);
+		return ((attributes.Length > 0)
+				&& (!string.IsNullOrEmpty(attributes[0].Description)))
+			? attributes[0].Description
+			: en.ToString();
+	}
+	
+	/// <summary>
+	/// Returns a list of enum values of the given type.
+	/// </summary>
+	/// <typeparam name="T">the enum type</typeparam>
+	/// <returns>the List of enum values</returns>
+	public static List<T> ListFromEnum<T>()
+	{
+		return Enum.GetValues(typeof(T)).Cast<T>().ToList();
+	}
+}
--- UNITY/Utils/readme.md
+++
@@ -1,5 +0,0 @@
-A lot of useful script for Unity or just C# you can compile it to make a dll lib et juste use .CS file directly in your project<br><br>
-
-
-# HOW TO USE:
-For Unity .Net 2.xx && 4.xx && IL2CPP or Visual studio all version
