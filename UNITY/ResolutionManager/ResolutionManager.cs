acb84e75a0f9cd88226ab5d494e023f5a6031841
Alexandre2BI <42917206+alex1600@users.noreply.github.com>
2021-09-08 10:48:17 +0200

    Update ResolutionManager.cs

--- UNITY/ResolutionManager/ResolutionManager.cs
+++ UNITY/ResolutionManager/ResolutionManager.cs
@@ -5,6 +5,7 @@ public class ResolutionManager : MonoBehaviour {
     public List<ResItem> resolutions = new List<ResItem>();
     public int currentResIndex = 0;
     private bool fullScreenMode;
+    
     //Link this method to UI > Dropdown > OnValueChanged
     public void SetResolution(int resIndex){
         currentResIndex = resIndex;
@@ -12,6 +13,7 @@ public class ResolutionManager : MonoBehaviour {
         // SaveManager.instance.resolutionIndex = resIndex;
         ApplyResolution();
     }
+    
     //Link this method to UI > Toggle > OnValueChanged
     public void SetFullScreenMode(bool fullScreen){
         fullScreenMode = fullScreen;
@@ -19,9 +21,11 @@ public class ResolutionManager : MonoBehaviour {
         // SaveManager.instance.fullScreenMode = fullScreen;
         ApplyResolution();
     }
+    
     public void ApplyResolution(){
         Screen.SetResolution(resolutions[currentResIndex].widht, resolutions[currentResIndex].height, fullScreenMode);
     }
+    
 }
 //List containing all resolutions
 [System.Serializable] public class ResItem{
