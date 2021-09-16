a6b08631d6a27bab418282795325053c9d4390c6
PsykoDev <45910905+PsykoDev@users.noreply.github.com>
2021-09-16 17:33:27 +0200

    Update FPSDisplay.cs

--- UNITY/FPSDisplay/Script/FPSDisplay.cs
+++ UNITY/FPSDisplay/Script/FPSDisplay.cs
@@ -9,13 +9,12 @@ using UnityEngine;
 public class FPSDisplay : MonoBehaviour {
     [SerializeField] private Rect startRect = new Rect(10, 10, 145, 40);
     [SerializeField] private float fps;
-    [SerializeField] private float MaxFPS = 0;
-    [SerializeField] private float MinFPS = 9999;
+    [SerializeField] private int MaxFPS = 0;
+    [SerializeField] private int MinFPS = 9999;
     public bool updateColor = true;
     public bool allowDrag = true;
     public float frequency = 0.5F;
     private float accum = 0f;
-    public int nbDecimal = 1;
     private int frames = 0;
     private Color color = Color.white;
     private GUIStyle style;
@@ -43,16 +42,16 @@ public class FPSDisplay : MonoBehaviour {
 
     void GetMinMax() {
         if(MaxFPS < fps)
-            MaxFPS = Mathf.Max(fps);
+            MaxFPS = (int)Mathf.Max(fps);
         if(MinFPS > fps)
-            MinFPS = Mathf.Min(fps);
+            MinFPS = (int)Mathf.Min(fps);
     }
 
     IEnumerator FPS() {
         while(true) {
             fps = accum / frames;
             color = (fps >= 30) ? new Color(0, 255, 0) : ((fps > 10) ? new Color(255, 0, 0) : new Color(255, 165, 0));
-            accum = 0.0F;
+            accum = 0;
             frames = 0;
             yield return new WaitForSeconds(frequency);
         }
@@ -69,7 +68,7 @@ public class FPSDisplay : MonoBehaviour {
     }
 
     void FPSWindow(int windowID) {
-        GUI.Label(new Rect(0, 0, startRect.width, startRect.height), $"{fps.ToString("f" + Mathf.Clamp(nbDecimal, 0, 10))} FPS\n[{MinFPS.ToString("f" + Mathf.Clamp(nbDecimal, 0, 10))}Min {MaxFPS.ToString("f" + Mathf.Clamp(nbDecimal, 0, 10))}Max]", style);
+        GUI.Label(new Rect(0, 0, startRect.width, startRect.height), $"{(int)fps} FPS\n[{MinFPS}Min {MaxFPS}Max]", style);
         if(allowDrag) GUI.DragWindow(new Rect(0, 0, Screen.width, Screen.height));
     }
 
